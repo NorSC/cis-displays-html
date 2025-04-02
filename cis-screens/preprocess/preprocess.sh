c1863a81a542f11f174f7434432c1892b8ad2fc9
Tom Bartindale <tom@bartindale.com>
2025-04-02 14:02:40 +0100

    ci

--- cis-screens/preprocess/preprocess.sh
+++ cis-screens/preprocess/preprocess.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-wget -O ./preprocessed.json $SCREENS_FILE
+# SCREENS_FILE="https://livenorthumbriaac-my.sharepoint.com/:u:/g/personal/tom_bartindale_northumbria_ac_uk/EdEqBv1wXy5MpfL8UyO7HfQBzoL8IogG6ujgfdBBr_ygmA?download=1"
+wget -O ./preprocessed.json "$SCREENS_FILE"
 # wget -O ./filenames.json $FILES_FILE
 sleep 1
 node ./preprocess.js
